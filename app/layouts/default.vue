f38e6fe7dcc4ed9fce13c19461c6dd539c88a570
Dog <104234930+dgxo@users.noreply.github.com>
2025-09-22 16:47:38 +0100

    Update layout colors to use neutral palette

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
 	<div
-		class="bg-gray-900 flex flex-col p-5 gap-0.5 min-h-screen text-gray-100"
+		class="bg-neutral-900 flex flex-col p-5 gap-0.5 min-h-screen text-neutral-100"
 	>
 		<slot />
 	</div>
